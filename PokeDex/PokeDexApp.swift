5e8b18d7e9be887f4495bc64c030b3fe6fb6e13d
yuya-3110 <ysuu45143745@gmail.com>
2025-03-10 23:17:25 +0900

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,18 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//  
+//  Created by Yuchinante on 2025/03/10
+//  
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
