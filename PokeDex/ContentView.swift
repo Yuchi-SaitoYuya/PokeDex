97657700d0d3f01709bbf05bf931da544454bcdf
yuya-3110 <ysuu45143745@gmail.com>
2025-03-14 23:55:11 +0900

    ContetViewをフラットにする

--- PokeDex/ContentView.swift
+++ PokeDex/ContentView.swift
@@ -9,17 +9,5 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
-#Preview {
-    ContentView()
 }
